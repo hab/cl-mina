3203ed90aef9734d3a5b43092de60a9bce3884b6
Nathan Sharpe <nathan@cliftonlabs.com>
2014-11-03 17:44:44 -0500

    Daemonize sidekiq

--- lib/cl_mina/tasks/sidekiq.rb
+++ lib/cl_mina/tasks/sidekiq.rb
@@ -4,7 +4,7 @@ namespace :sidekiq do
     queue %{cd #{deploy_to}/#{current_path}}
 
     queue %{echo "-----> Start sidekiq"}
-    queue echo_cmd "bundle exec sidekiq -c #{sidekiq_concurrency} -e #{rails_env} -L #{sidekiq_log} -P #{sidekiq_pid}"
+    queue echo_cmd "bundle exec sidekiq -d -c #{sidekiq_concurrency} -e #{rails_env} -L #{sidekiq_log} -P #{sidekiq_pid}"
   end
 
   desc "Quiet sidekiq"
