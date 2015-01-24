5935d82c9a359efb5c5f219a240e891d4838a3f0
Hakon Baunsgaard <hakonb@gmail.com>
2015-01-24 20:04:17 +0100

    Updated logrotate to my preferences

--- lib/cl_mina/tasks/logrotate.rb
+++ lib/cl_mina/tasks/logrotate.rb
@@ -2,7 +2,7 @@ namespace :logrotate do
   desc "Setup logrotate for all log files in log directory"
   task :setup do
     template "logrotate.erb", "/tmp/logrotate"
-    queue echo_cmd "sudo mv /tmp/logrotate /etc/logrotate.d/#{application}_#{stage}"
+    queue echo_cmd "sudo mv /tmp/logrotate /etc/logrotate.d/#{application}_#{deploy_server}"
   end
   after "deploy:setup", "logrotate:setup"
 end
